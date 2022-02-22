5ffa7c19408c524933d587a7eefda267d8404822
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-02-22 21:46:27 +0330

    fix: add data fake

---
+++ src/dummyData.js
@@ -0,0 +1,233 @@
+export const data = [
+  {
+    name: "Feb",
+    "Active User": 4000,
+  },
+  {
+    name: "Mar",
+    "Active User": 6000,
+  },
+  {
+    name: "Apr",
+    "Active User": 2000,
+  },
+  {
+    name: "May",
+    "Active User": 3000,
+  },
+  {
+    name: "Jun",
+    "Active User": 5000,
+  },
+  {
+    name: "Agu",
+    "Active User": 2000,
+  },
+  {
+    name: "Sep",
+    "Active User": 5000,
+  },
+  {
+    name: "Oct",
+    "Active User": 2000,
+  },
+  {
+    name: "Nov",
+    "Active User": 1000,
+  },
+  {
+    name: "Desc",
+    "Active User": 5000,
+  },
+];
+
+export const productData = [
+  {
+    name: "Feb",
+    Sales: 4000,
+  },
+  {
+    name: "Mar",
+    Sales: 6000,
+  },
+  {
+    name: "Apr",
+    Sales: 2000,
+  },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 2,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 3,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 4,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 5,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 6,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 7,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 8,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 9,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+  {
+    id: 10,
+    username: "John snow",
+    avatar:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    email: "node@gmail.com",
+    status: "active",
+    transaction: "$123",
+  },
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 2,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 3,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 4,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 5,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 6,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 7,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 8,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 9,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+  {
+    id: 10,
+    name: "Airphone Airpads",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    stock: 123,
+    status: "active",
+    price: "$123",
+  },
+];
