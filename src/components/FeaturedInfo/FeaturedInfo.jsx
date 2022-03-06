8d879ce2041c6693732009bf74c0ed7e5ca3f0c3
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-03-06 10:01:30 +0330

    fix: add get data

--- src/components/FeaturedInfo/FeaturedInfo.jsx
+++ src/components/FeaturedInfo/FeaturedInfo.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 import "./FeaturedInfo.style.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { userRequest } from "../../services/axios/axios.config";
 const FeaturedInfo = () => {
+  const [incoms, setIncoms] = useState([]);
+  const [precent, setPercent] = useState(null);
+  useEffect(() => {
+    const getIncome = async () => {
+      const { data } = await userRequest.get("/orders/income");
+      setIncoms(data);
+      setPercent((data[0]?.total * 100) / data[1]?.total - 100);
+    };
+    getIncome();
+  }, []);
+  // console.log(incoms[1].total);
   return (
     <div className="featured">
       <div className="featuredItem">
         <div className="featuredTitle">Ravanue</div>
         <div className="featuredMoneyContainer">
-          <div className="featuredMoney">$2.415</div>
+          <div className="featuredMoney">${incoms[0]?.total}</div>
           <div className="featuredMonyRate">
-            -11.4 <ArrowDownward className="featuredIcon negative"/>
+            {precent < 0 ? (
+              <>
+                ${precent} <ArrowDownward className="featuredIcon negative" />
+              </>
+            ) : (
+              <>
+                ${precent} <ArrowDownward className="featuredIcon negative" />
+              </>
+            )}
           </div>
         </div>
         <span className="featuredsub">Compared to last month</span>
@@ -19,7 +41,7 @@ const FeaturedInfo = () => {
         <div className="featuredMoneyContainer">
           <div className="featuredMoney">$4.415</div>
           <div className="featuredMonyRate">
-            -1.4 <ArrowDownward className="featuredIcon negative"/>
+            -1.4 <ArrowDownward className="featuredIcon negative" />
           </div>
         </div>
         <span className="featuredsub">Compared to last month</span>
