c3651d9130076ed174d3cf82c151d5dba8ec6380
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-03-06 09:57:52 +0330

    fix: add dynminc data in chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import Chart from "../../components/Chart/Chart";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
 import WidgetLg from "../../components/WidgetLg/WidgetLg";
 import Widgetsm from "../../components/Widgetsm/Widgetsm";
-import { data } from "../../dummyData";
+import { userRequest } from "../../services/axios/axios.config";
 
 import "./Home.style.css";
 const Home = () => {
+  const [userStates, setUserStates] = useState([]);
+  const MONTH = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+  useEffect(() => {
+    const getUserStates = async () => {
+      const { data } = await userRequest.get("/users/states");
+      data.map((user) => {
+        setUserStates((prev) => [
+          ...prev,
+          { name: MONTH[user._id - 1], "Active User": user.total },
+        ]);
+      });
+    };
+    getUserStates();
+  }, [MONTH]);
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart
-        data={data}
+        data={userStates}
         dataKey={"Active User"}
         grid
         title={"User Analyices"}
