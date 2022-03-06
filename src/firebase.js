3596d1aaceaa7603c4e967d35614cb4b1e742c6f
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-03-06 09:51:49 +0330

    fix: install firebase covert url

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBV3FBth5pmgeuHjc2_PBV_Umtv4JIXu-I",
+  authDomain: "panel-shoping.firebaseapp.com",
+  projectId: "panel-shoping",
+  storageBucket: "panel-shoping.appspot.com",
+  messagingSenderId: "671148159032",
+  appId: "1:671148159032:web:06c1acf411407a7b80f6cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
