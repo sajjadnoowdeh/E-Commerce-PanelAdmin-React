aff5a2ca1d6714ed1629db0be4e84796c62b2b20
sajjad noowdeh <sajjad.noowdeh91@gmail.com>
2022-03-09 19:09:55 +0330

    fix: clean code

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import TopBar from "../../components/TopBar/TopBar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { Outlet, Routes, Route, useNavigate } from "react-router-dom";
-
-import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const Layout = () => {
